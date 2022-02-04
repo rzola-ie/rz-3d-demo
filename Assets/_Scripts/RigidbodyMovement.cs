3fcdd38bdc006593aa94182738a43596ea208375
rzola-ie <ryan.zola@idea-evolver.com>
2022-02-04 12:20:10 -0500

    just sliding door things

--- Assets/_Scripts/RigidbodyMovement.cs
+++ Assets/_Scripts/RigidbodyMovement.cs
@@ -4,54 +4,65 @@ using UnityEngine;
 
 public class RigidbodyMovement : MonoBehaviour
 {
-    private Vector3 PlayerMovementInput;
-    private Vector2 PlayerMouseInput;
-    private float xRot;
+  private Vector3 PlayerMovementInput;
+  private Vector2 PlayerMouseInput;
+  private float xRot;
 
-    [SerializeField] private LayerMask FloorMask;
-    [SerializeField] private Transform FeetTransform;
-    [SerializeField] private Transform PlayerCamera;
-    [SerializeField] private Rigidbody PlayerBody;
-    [Space]
-    [SerializeField] private float Speed;
-    [SerializeField] private float Sensitivity;
-    [SerializeField] private float JumpForce;
+  [SerializeField] private LayerMask FloorMask;
+  [SerializeField] private Transform FeetTransform;
+  [SerializeField] private Transform PlayerCamera;
+  [SerializeField] private Rigidbody PlayerBody;
+  [Space]
+  [SerializeField] private float Speed;
+  [SerializeField] private float Sensitivity;
+  [SerializeField] private float JumpForce;
 
-    private void Start()
+  private void Start()
+  {
+    Cursor.lockState = CursorLockMode.Locked;
+  }
+
+
+  // Update is called once per frame
+  void Update()
+  {
+    PlayerMovementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+    PlayerMouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+    MovePlayer();
+    MovePlayerCamera();
+  }
+
+  private void MovePlayer()
+  {
+    Vector3 MoveVector = transform.TransformDirection(PlayerMovementInput) * Speed;
+    PlayerBody.velocity = new Vector3(MoveVector.x, PlayerBody.velocity.y, MoveVector.z);
+
+    if (Input.GetKeyDown(KeyCode.Space))
     {
-        Cursor.lockState = CursorLockMode.Locked;
+      if (Physics.CheckSphere(FeetTransform.position, 0.1f, FloorMask))
+      {
+        PlayerBody.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+      }
     }
+  }
 
+  private void MovePlayerCamera()
+  {
+    xRot -= PlayerMouseInput.y * Sensitivity;
 
-    // Update is called once per frame
-    void Update()
+    transform.Rotate(0f, PlayerMouseInput.x * Sensitivity, 0f);
+    PlayerCamera.transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+  }
+
+  private void OnTriggerStay(Collider other)
+  {
+    if (other.tag == "Door")
     {
-        PlayerMovementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-        PlayerMouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-
-        MovePlayer();
-        MovePlayerCamera();
+      if (other.GetComponent<AutomaticDoor>().Moving == false)
+      {
+        other.GetComponent<AutomaticDoor>().Moving = true;
+      }
     }
-
-    private void MovePlayer()
-    {
-        Vector3 MoveVector = transform.TransformDirection(PlayerMovementInput) * Speed;
-        PlayerBody.velocity = new Vector3(MoveVector.x, PlayerBody.velocity.y, MoveVector.z);
-
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            if(Physics.CheckSphere(FeetTransform.position, 0.1f, FloorMask)) 
-            {
-                PlayerBody.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
-            }
-        }
-    }
-
-    private void MovePlayerCamera()
-    {
-        xRot -= PlayerMouseInput.y * Sensitivity;
-
-        transform.Rotate(0f, PlayerMouseInput.x * Sensitivity, 0f);
-        PlayerCamera.transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
-    }
+  }
 }
